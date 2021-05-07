b46887c38ee3aa152c6601a27e8d47c9b8c534e0
ramansharma0811 <66987011+ramansharma0811@users.noreply.github.com>
2021-05-08 00:36:46 +0530

    Add files via upload

--- model_test.py
+++ model_test.py
@@ -36,8 +36,8 @@ def predict_diabetes(data: PredictionSystem):
     
     
 if __name__ == '__main__':
-    port=int(os.environ.get("PORT",8000))
-    uvicorn.run(app,host='127.0.0.1',port=port)
+    port=int(os.environ.get("PORT",5000))
+    uvicorn.run(app,host='0.0.0.0',port=port)
     
     
 
